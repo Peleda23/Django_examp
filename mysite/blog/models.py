0bc3906e40561871a8513d607fdad5af0a77e289
peleda23 <kosmosas23@gmail.com>
2024-12-12 20:43:34 +0200

    43 psl.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super().get_queryset().filter(status=Post.Status.PUBLISHED))
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -16,6 +22,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
+    objects = models.Manager()
+    published = PublishedManager()
+    
     class Meta:
         ordering = ['-publish']
         indexes = [
