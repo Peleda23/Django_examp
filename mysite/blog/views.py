dc0f3d5b7ede733574d1bdc99b89e2f6a7ead3aa
Peleda23 <kosmosas23@gmail.com>
2024-12-20 14:38:49 +0200

    Pridejom Paginator viewsose kuris leidzia deti postus per kelis puslapius ir sukureme templata kad tarp ju vaiscioti.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,32 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    
+    # Norodome kad rodysime tik 3 postus viename puslapyje.
+    
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    
+    
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, "blog/post/detail.html", {"post": post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
