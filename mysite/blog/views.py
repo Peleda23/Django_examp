90f95995c3cb28e95917d8c60ccb5527f7fd829d
peleda23 <kosmosas23@gmail.com>
2024-12-21 07:35:52 +0200

    Added class type view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,14 @@
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView
 from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -13,6 +20,11 @@ def post_list(request):
     page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
+    
+    # Jei page_number nera skaicius, graziname pirmaji puslapi
+    except PageNotAnInteger:
+        posts = paginator.page(1)    
+        
     except EmptyPage:
         # Jei page_number didesnis nei paginator.num_pages, graziname paskutini puslapi
         
